9cf500c112ba2a1058418c6cad80e969ebfd8a9d
Henry Tran <henrytran07@g.ucla.edu>
2026-08-08 09:41:51 +0700

    add tix source code and use api in between main page ticketmaster page and ticket page

---
+++ source/main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from ticket import router as ticket_router
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR / "templates"
+
+app = FastAPI()
+
+app.mount("/css", StaticFiles(directory=BASE_DIR / "css"), name="css")
+app.mount("/images", StaticFiles(directory=BASE_DIR / "images"), name="images")
+app.include_router(ticket_router)
+
+
+@app.get("/")
+def main():
+    return FileResponse(TEMPLATE_DIR / "main.html")
+
+
+@app.get("/ticket-master")
+def ticketmaster():
+    return FileResponse(TEMPLATE_DIR / "ticketmaster.html")
+
+# @app.get("/ticket")
+# def ticketmaster():
+#     return FileResponse(TEMPLATE_DIR / "ticket.html")
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse(BASE_DIR / "images" / "ucla.png")
